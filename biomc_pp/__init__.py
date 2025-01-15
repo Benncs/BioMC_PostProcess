956bf69c99f8374ed08f05f56ae74e91f83f98e9
Casale Benjamin <casale@insa-09720>
2025-01-15 12:29:54 +0100

    fix: import when building rust/pythhon with  maturin

--- biomc_pp/__init__.py
+++ biomc_pp/__init__.py
@@ -1,8 +1,15 @@
 
-from biomc_pp import biomc_pp
+
+from .biomc_pp import *
 import numpy as np
 FIGURE_TYPE=".png"
 TIME_UNIT ="s"
+
+__all__ = []
+__doc__ = biomc_pp.__doc__
+if hasattr(biomc_pp, "__all__"):
+    __all__ = biomc_pp.__all__
+
 
 def set_time_unit_to_hour():
     global TIME_UNIT
@@ -12,7 +19,7 @@ def get_time_unit()->str:
     return TIME_UNIT
 
 
-def check_time_unit(results: biomc_pp.PostProcess)->np.ndarray:
+def check_time_unit(results: PostProcess)->np.ndarray:
     # Conversion to hour if duration too long
     if results.time[-1] > 10000:
         t = np.array(results.time) / 3600.0
@@ -23,4 +30,12 @@ def check_time_unit(results: biomc_pp.PostProcess)->np.ndarray:
 
 
 def get_post_process(name:str,root:str="./results"):
-    return biomc_pp.PostProcess(name,root)
+    return PostProcess(name,root)
+
+
+__all__.extend([
+    "set_time_unit_to_hour",
+    "get_time_unit",
+    "check_time_unit",
+    "get_post_process",
+])
